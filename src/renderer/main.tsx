c54aef65a74fe564a2330968be46b4514386949f
leonfox28 <yuanzhe.hu@icloud.com>
2026-04-12 12:09:55 +0800

    feat: switch from hardcoded CSS variables to JS theme injection

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { darkPlusTheme } from '@shared/config/theme.config'
+import { applyTheme } from './utils/theme'
 import './styles/global.css'
+
+// Apply theme CSS variables before React renders to avoid FOUC
+applyTheme(darkPlusTheme)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
